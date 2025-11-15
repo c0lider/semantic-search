c266a0a6f8c77a9cb2b136191b879cac020dea7c
Oliver Seiffermann <oliver.seiffermann@stud.fra-uas.de>
2025-11-15 16:06:03 +0100

    feat: implement index controller + base template structure

---
+++ src/Controller/IndexController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Controller;
+
+use Pimcore\Controller\FrontendController;
+use Symfony\Bridge\Twig\Attribute\Template;
+
+class IndexController extends FrontendController
+{
+    #[Template(template: 'documents/index.html.twig')]
+    public function indexAction()
+    {
+        return [];
+    }
+}
